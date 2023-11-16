b23488b966dab1cd3af96aa9472041ca434b5646
liyuanmay <504104685@qq.com>
2023-11-16 09:18:37 -0700

    bug fix

--- src/arches-v2/units/unit-tp.cpp
+++ src/arches-v2/units/unit-tp.cpp
@@ -186,6 +186,7 @@ FREE_INSTR:
 			i_req.port = _tp_index;
 			i_req.paddr = _pc & ~0x3full;
 			i_req.type = MemoryRequest::Type::LOAD;
+			i_req.size = CACHE_BLOCK_SIZE;
 			inst_cache->write_request(i_req, i_req.port);
 			_i_buffer.reqData = true;
 			_i_buffer.getData = false;
