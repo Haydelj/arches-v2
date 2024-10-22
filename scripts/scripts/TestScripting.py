65b9fae3029977c0ee1036f5a62f0849390c93af
pommpy <hpommpy@gmail.com>
2024-10-21 22:15:51 -0600

    WIP

--- scripts/scripts/TestScripting.py
+++ scripts/scripts/TestScripting.py
@@ -7,15 +7,17 @@ import utils
 from matplotlib import pyplot as plt, ticker as mticker, colors as mcolors
 from math import pi, sqrt, exp
 import numpy as np
+import itertools
 
-Run = True
+Run = False
+clear_folder = False
 ## Do your config here
 ## config[name -> str] = str list
 output_folder_name = r"TestScripting"
 configs = {}
 configs["simulator"] = ["trax", "dual-streaming"]
 configs["scene_name"] = ["sponza", "san-miguel"]
-configs["size"] = ["128", "512"]
+configs["size"] = ["1024"]
 
 
 last_level_folder = os.path.basename(current_file_dir)
@@ -26,62 +28,86 @@ abs_exe_path = os.path.join(current_file_dir, r"..\..\build\src\arches-v2\Releas
 abs_output_dir = os.path.join(current_file_dir, r"..\results", output_folder_name)
 
 if Run:
-    exp_instance = ExperimentInstance(abs_output_dir, abs_exe_path, clear_folder = True)
+    exp_instance = ExperimentInstance(abs_output_dir, abs_exe_path, clear_folder = clear_folder)
     # Let's run simulations and generate some results
     exp_instance.run(configs=configs)
 
-quit()
+# quit()
 # Let's try to visualize the logs
 
 # Example 1: Comparing Mrays/s for trax and dual-streaming on 2 scenes
+# This will be a histogram
+# data_name = "MRays/s"
+# vis_configs = configs
+# vis_configs["size"] = ["512"]
+# config_names = [key for key, value in vis_configs.items()]
+# lists = vis_configs.values()
+# combinations = itertools.product(*lists)
 
-
-
-
-# Example 2: Visualize the bandwidth of two simulator on san-miguel
+# for combo in combinations:
+#     config = dict(zip(config_names, combo))
+#     filename = utils.HashFilename(config) + ".txt"
+#     file_path = os.path.join(abs_output_dir, filename)
+#     data = utils.ExtractLogInfo(file_path, data_name)
+#     print(config, data)
+# quit()
+# Example 2: Visualize the bandwidth of two simulators on san-miguel
+# This will be some charts containing the time-varying curves
 config = {}
-config["scene_name"] = "san-miguel"
-config["size"] = "512"
-
+config["scene_name"] = "sponza"
+config["size"] = "1024"
 config["simulator"] = "trax"
+memory_unit_name = "L2 Cache"
 filename = utils.HashFilename(config) + ".txt"
 file_path = os.path.join(abs_output_dir, filename)
-trax_cycles, trax_bandwidth_total, trax_bandwidth_details = utils.GetBandwidthList(file_path, "DRAM")
+trax_bandwidths = utils.GetBandwidth(file_path, memory_unit_name)
+trax_cycles = trax_bandwidths[0]
+trax_total_bandwidth = trax_bandwidths[1]
+trax_detailed_bandwidth = trax_bandwidths[2]
 
 config["simulator"] = "dual-streaming"
 filename = utils.HashFilename(config) + ".txt"
 file_path = os.path.join(abs_output_dir, filename)
-ds_cycles, ds_bandwidth_total, ds_bandwidth_details = utils.GetBandwidthList(file_path, "DRAM")
-
+ds_bandwidths = utils.GetBandwidth(file_path, memory_unit_name)
+ds_cycles = ds_bandwidths[0]
+ds_total_bandwidth = ds_bandwidths[1]
+ds_detailed_bandwidth = ds_bandwidths[2]
 smoothing = True
-def gauss(n=11,sigma=1):
-    r = range(-int(n/2),int(n/2)+1)
-    return [1 / (sigma * sqrt(2*pi)) * exp(-float(x)**2/(2*sigma**2)) for x in r]
-kernel_size = 61
-kernel = np.array(gauss(kernel_size, 5))
-kernel = kernel / kernel.sum()
+trax_kernel_size = min(31, len(trax_total_bandwidth) - 1)
+trax_kernel = np.array(utils.gauss(trax_kernel_size, 5))
+trax_kernel = trax_kernel / trax_kernel.sum()
 
-print(trax_cycles)
-print(trax_bandwidth_total)
-quit()
+ds_kernel_size = min(31, len(ds_total_bandwidth) - 1)
+ds_kernel = np.array(utils.gauss(ds_kernel_size, 5))
+ds_kernel = ds_kernel / ds_kernel.sum()
+
 # Let's use 3 plots (1 for comparison, 1 for trax, 1 for ds)
-plt.figure(figsize=(12, 4))
-
+plt.figure(figsize=(24, 8))
 plt.subplot(1, 3, 1)
-y = trax_bandwidth_total
+y = trax_total_bandwidth
 if smoothing:
-    y = np.convolve(y, kernel, mode='same')
+    y = np.convolve(y, trax_kernel, mode='same')
 plt.plot(trax_cycles, y, label = "trax")
-
-y = ds_bandwidth_total
+y = ds_total_bandwidth
 if smoothing:
-    y = np.convolve(y, kernel, mode='same')
+    y = np.convolve(y, ds_kernel, mode='same')
 plt.plot(ds_cycles, y, label = "dual-streaming")
-
-
+plt.title("Trax/Dual-Streaming Bandwidth Comparison")
+plt.legend()
 
 plt.subplot(1, 3, 2)
+plt.title("Trax {} Bandwidth Utilization on {}".format(memory_unit_name, config["scene_name"]))
+for (unit_name, request_label), bandwidths in trax_detailed_bandwidth.items():
+    if len(bandwidths) == len(trax_cycles):
+        smoothed = np.convolve(bandwidths, trax_kernel, mode='same')
+        plt.plot(trax_cycles, smoothed, label=f"{unit_name} - {request_label}")
+plt.legend()
 
 plt.subplot(1, 3, 3)
+plt.title("Dual-Streaming {} Bandwidth Utilization on {}".format(memory_unit_name, config["scene_name"]))
+for (unit_name, request_label), bandwidths in ds_detailed_bandwidth.items():
+    if len(bandwidths) == len(ds_cycles):
+        smoothed = np.convolve(bandwidths, ds_kernel, mode='same')
+        plt.plot(ds_cycles, smoothed, label=f"{unit_name} - {request_label}")
 plt.legend()
 plt.show()
--- scripts/utils.py
+++ scripts/utils.py
@@ -2,6 +2,7 @@ import json
 import hashlib
 import os
 import sys, re
+from math import pi, sqrt, exp
 
 # return a hashed string given a dict
 def HashFilename(config: dict) -> str:
@@ -12,17 +13,30 @@ def HashFilename(config: dict) -> str:
     hash_string = hash_object.hexdigest()
     return hash_string
 
-# Here we extract the scaler data (e.g., Mrays/s, Power, etc.) using regular expression
+# Here we extract the scaler data (e.g., Mrays/s, Power, etc.) using regular expressions
 def ExtractLogInfo(log_path: str, data_name: str):
-    if not os.path.exists(log_path):
-        raise Exception("Exe {} does not exist".format(log_path))
-    pass
-
-def GetBandwidthList(log_path: str, unit_name: str) -> dict:
-    cycles = []
-    total_bandwidth = []
+    pattern = r"{}:\s*(\d+)".format(data_name)
+    find = False
+    value = 0
     with open(log_path) as f:
-        all_pairs = {}
+        lines = f.readlines()  # List containing lines of file
+        for line in lines:
+            match = re.search(pattern, line, re.IGNORECASE)
+            if match:
+                find = True
+                value = float(match.group(1))
+    if not find:
+        raise Exception("Data {} does not exist in log {}.".format(data_name, log_path))
+    return value
+        
+def GetBandwidth(log_path: str, unit_name: str) -> list:
+    cycles = []
+    total_bandwidth_timeline = []
+    total_bandwidth_average = 0
+    with open(log_path) as f:
+        all_pairs_timeline = {} # value is a list
+        all_pairs_average = {} # value is a float
+
         lines = f.readlines()  # List containing lines of file
         # Find all pairs at the first iteration
         header_regex = r'-+{}-+'.format(unit_name)
@@ -40,21 +54,22 @@ def GetBandwidthList(log_path: str, unit_name: str) -> dict:
                 length += 1
             if under_label and line == "Bandwidth Utilization Starts:\n":
                 under_detailed = True
-                # print("Start!")
             match = re.search(detail_pattern, line)
             if under_detailed and match:
                 unit_name = match.group(1)
                 request_label = match.group(2)
                 bandwidth = float(match.group(3))
                 tot += bandwidth
-                all_pairs[(unit_name, request_label)] = []
+                all_pairs_timeline[(unit_name, request_label)] = []
 
             if under_label and under_detailed and line == "Bandwidth Utilization Ends.\n":
                 under_label = False
                 under_detailed = False
-                total_bandwidth.append(tot)
-                print(total_bandwidth)
-                # print("tot = ", tot)
+                
+                if len(total_bandwidth_timeline) < len(cycles):
+                    total_bandwidth_timeline.append(tot)
+                else:
+                    total_bandwidth_average = tot
                 tot = 0
 
         # Fill out the lists
@@ -65,9 +80,10 @@ def GetBandwidthList(log_path: str, unit_name: str) -> dict:
             if re.search(header_regex, line):
                 under_label = True
                 t += 1
-                for key in all_pairs:
-                    all_pairs[key].append(float(0))
-            if under_label and line == "Bandwidth Utilization Starts:":
+                if t < len(cycles):
+                    for key in all_pairs_timeline:
+                        all_pairs_timeline[key].append(float(0))
+            if under_label and line == "Bandwidth Utilization Starts:\n":
                 under_detailed = True
 
             match = re.search(detail_pattern, line)
@@ -75,10 +91,17 @@ def GetBandwidthList(log_path: str, unit_name: str) -> dict:
                 unit_name = match.group(1)
                 request_label = match.group(2)
                 bandwidth = float(match.group(3))
-                all_pairs[(unit_name, request_label)][t] = bandwidth
+                if t < len(cycles):
+                    all_pairs_timeline[(unit_name, request_label)][t] = bandwidth
+                else:
+                    all_pairs_average[(unit_name, request_label)] = bandwidth
                 
-            if under_label and under_detailed and line == "Bandwidth Utilization Ends.":
+            if under_label and under_detailed and line == "Bandwidth Utilization Ends.\n":
                 under_label = False
                 under_detailed = False
-        return cycles, total_bandwidth, all_pairs
+        return [cycles, total_bandwidth_timeline, all_pairs_timeline, total_bandwidth_average, all_pairs_average]
     raise Exception("File {} doesn't exist.".format(log_path))
+
+def gauss(n=11,sigma=1):
+    r = range(-int(n/2),int(n/2)+1)
+    return [1 / (sigma * sqrt(2*pi)) * exp(-float(x)**2/(2*sigma**2)) for x in r]
